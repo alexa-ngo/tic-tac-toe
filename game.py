69f7ff913d16227acade78361e2e1bf8c772d2be
Alexa <alexaariango@outlook.com>
2024-12-15 20:25:23 -0800

    Updates

--- game.py
+++ game.py
@@ -16,38 +16,32 @@ class Game:
         if (b.get_pos(0) == "o" and b.get_pos(1) == "o" and b.get_pos(2) == "o" or
                 b.get_pos(3) == "o" and b.get_pos(4) == "o" and b.get_pos(5) == "o" or
                 b.get_pos(6) == "o" and b.get_pos(7) == "o" and b.get_pos(8) == "o"):
-            print("6")
             return "o wins!"
 
         # X wins for horizontal lines
         if (b.get_pos(0) == "x" and b.get_pos(1) == "x" and b.get_pos(2) == "x" or
                 b.get_pos(3) == "x" and b.get_pos(4) == "x" and b.get_pos(5) == "x" or
                 b.get_pos(6) == "x" and b.get_pos(7) == "x" and b.get_pos(8) == "x"):
-            print("7")
             return "x wins!"
 
         # O wins for vertical lines
         if (b.get_pos(0) == "o" and b.get_pos(3) == "o" and b.get_pos(6) == "o" or
                 b.get_pos(1) == "o" and b.get_pos(4) == "o" and b.get_pos(7) == "o" or
                 b.get_pos(2) == "o" and b.get_pos(5) == "o" and b.get_pos(8) == "o"):
-            print("8")
             return "o wins!"
 
         # X wins for vertical lines
         if (b.get_pos(0) == "x" and b.get_pos(3) == "x" and b.get_pos(6) == "x" or
                 b.get_pos(1) == "x" and b.get_pos(4) == "x" and b.get_pos(7) == "x" or
                 b.get_pos(2) == "x" and b.get_pos(5) == "x" and b.get_pos(8) == "x"):
-            print("9")
             return "x wins!"
 
         # O wins for diagonals
         if (b.get_pos(0) == "o" and b.get_pos(4) == "o" and b.get_pos(8) == "o" or
                 b.get_pos(2) == "o" and b.get_pos(4) == "o" and b.get_pos(6) == "o"):
-            print("10")
             return "o wins!"
 
         # X wins for diagonals
         if (b.get_pos(0) == "x" and b.get_pos(4) == "x" and b.get_pos(8) == "x" or
                 b.get_pos(2) == "x" and b.get_pos(4) == "x" and b.get_pos(6) == "x"):
-            print("10")
             return "x wins!"
--- main.py
+++ main.py
@@ -6,14 +6,14 @@ alexa = Player("x")
 riley = Player("o")
 
 the_board = Board(alexa, riley)
-the_board.set_pos(1)
 the_board.set_pos(0)
-the_board.new_round()
 the_board.set_pos(2)
-the_board.set_pos(4)
 the_board.new_round()
-the_board.set_pos(5)
+the_board.set_pos(4)
+the_board.set_pos(6)
+the_board.new_round()
 the_board.set_pos(8)
+the_board.set_pos(7)
 the_board.new_round()
 
 
